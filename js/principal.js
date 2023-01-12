54c2168d72d37870bc1bce1f9346fe3f280efcf4
joaoAugustoParaty <joaodrow@gmail.com>
2023-01-12 12:46:53 -0300

    third

--- js/principal.js
+++ js/principal.js
@@ -19,12 +19,14 @@ if (peso <= 0 || peso >= 200) {
     console.log("Peso inválido!");
     pesoEhValido = false;
     tdImc.textContent = "Peso Inválido";
+    paciente.classList.add("paciente-invalido");
 }
 
 if (altura <= 0 || altura >= 3.00) {
     console.log("Altura inválida!");
     alturaEhValida = false;
-    tdImc.textContent = "Altura Inválida"
+    tdImc.textContent = "Altura Inválida";
+    paciente.classList.add("paciente-invalido");
 }
 
 if (alturaEhValida && pesoEhValido) {
